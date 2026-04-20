9d62aacf8d5240fe0ff6dd6b5357ba32eac3d56c
Stef-aniya <stefaniyamomot@gmail.com>
2026-04-20 17:06:40 +0300

    7

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,14 +1,17 @@
-import css from "./LayoutNotes.module.css";
-type Props = {
-  children: React.ReactNode;
-  sidebar: React.ReactNode;
-};
-const Layout = ({ children, sidebar }: Props) => {
-  return (
-    <section className={css.container}>
-      <aside className={css.sidebar}>{sidebar}</aside>
-      <div className={css.content}>{children}</div>
-    </section>
-  );
-};
-export default Layout;
+// import css from "./LayoutNotes.module.css";
+// type Props = {
+//   children: React.ReactNode;
+//   sidebar: React.ReactNode;
+// };
+// const Layout = ({ children, sidebar }: Props) => {
+//   return (
+//     <section className={css.container}>
+//       <aside className={css.sidebar}>{sidebar}</aside>
+//       <div className={css.content}>{children}</div>
+//     </section>
+//   );
+// };
+// export default Layout;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
